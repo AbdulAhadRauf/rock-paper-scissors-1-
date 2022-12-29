31415b5d77c26a90482b81b41a637eaa327a7447
Abdul Ahad Rauf <120673653+AbdulAhadRauf@users.noreply.github.com>
2022-12-29 19:39:50 +0000

    Play Rock Paper and Scissors all the time with the code until you want  to Stop

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import random
-computer = random.choice(['R', 'P', 'S'])
-user=input('Rock,Paper,Scissor \n Enter R,P,S')
-Flag= False
-if user == computer:
-  print("Draw")
-elif user == "R" and computer =="P" or user =="P" and computer=="S" or user =="S" and computer=="R" :
-  Flag=True
-else:
-  Flag=False
+Losses=0
+Wins=0
+Draws=0
+rules=['R', 'P', 'S']
+Play_again = "Y"
+while (Play_again == "Y"):
+  computer = random.choice(rules)
+  user=input('Rock,Paper,Scissor\nEnter R,P,S\n')
+  user = user.upper()
+  if user not in rules:
+    print("It seems you entered a different value, Please consider Entering the r,p,s only")
+    break
+  Flag= False
+  if user == computer:
+    print("Draw\n")
+    Draws=Draws+1
+  elif user == "R" and computer =="P" or user =="P" and computer=="S" or user =="S" and computer=="R" :
+    Flag=True
+    Losses=Losses+1
+  else:
+    Flag=False
+    Wins=Wins+1
+  
+  if Flag:
+    print(f'C = {computer}, You = {user} You Loose\n')
+  else:
+    print(f'C = {computer}, You = {user} You win\n')
+  Play_again = input("You want to play again?\nY or N\n").upper()
 
-if Flag:
-  print(f'C = {computer}, You = {user} You Loose')
-else:
-  print(f'C = {computer}, You = {user} You win')
+if Play_again == "N":
+  print(f'{Wins} Wins, {Losses} Losses, {Draws} Draws')
